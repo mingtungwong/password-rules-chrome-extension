2d63828ead1ca50eafa00398db4e5ce53ed35420
Ming Tung Wong <mingtungwong@outlook.com>
2017-08-20 13:29:10 -0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+const apiURL = 'http://api.password-rules.myrandomcode.com/site/';
+
+function getCurrentTabHost(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+        const tab = tabs[0];
+        const host = new URL(tab.url).hostname;
+
+        callback(host.indexOf('www.') >= 0 ? host.split('www.')[1] : host);
+    })
+}
+
+function renderRules(data) {
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getCurrentTabHost(function(host) {
+        fetch(`${apiURL}${host}`, {mode: 'cors'})
+        .then(function(response) {
+            response.json().then(function(data) {
+                renderRules(data[0]);
+            })
+        })
+    })
+})
